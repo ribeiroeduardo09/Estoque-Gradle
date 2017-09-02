7d45a1cd8b6f15f1f9fa6f0f681278982b7939cb
Eduardo Pereira Ribeiro <ribeiroeduardo09@gmail.com>
2017-09-01 22:53:58 -0300

    Teste

--- src/main/java/hello/Testcase.java
+++ src/main/java/hello/Testcase.java
@@ -1,16 +1,16 @@
 package hello;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.time.LocalDate;
 import java.time.Month;
 
-public class Teste {
-	
+import org.junit.Test;
 
-	public static void main(String[] args){
-	
-		
+public class Testcase {
+
+	@Test
+	public void test() {
 		Produto p1 = new Produto("Teclado", 15.50f, 20.50f, 111111, "Teclado comum", 3, LocalDate.of(2017, Month.FEBRUARY, 10));
 		Produto p2 = new Produto("Monitor 15 Pol.", 234.90f, 310.50f, 222222, "Monitor LG 15 Polegadas", 5, LocalDate.of(2017, Month.FEBRUARY, 28));
 			
@@ -25,7 +25,10 @@ public class Teste {
 	
 		assertEquals(sist.getProdutos().get(0).getNome(), "Teclado");
 		
+		Usuario u1 = new Usuario("Eduardo", "ribeiroeduardo09", "senha123", 13729, "Desenvolvedor", LocalDate.of(2017, Month.SEPTEMBER, 01));
+		Usuario u2 = new Usuario("Ruben", "rubenfilipeao", "senha321", 14582, "Desenvolvedor", LocalDate.of(2017, Month.SEPTEMBER, 01));
 		
 		
 	}
+
 }
