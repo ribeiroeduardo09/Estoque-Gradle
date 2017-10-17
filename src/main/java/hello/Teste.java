fa513a4f4173e68e56b68b7b3dd286754cd10fd1
Eduardo Pereira Ribeiro <ribeiroeduardo09@gmail.com>
2017-10-16 22:20:35 -0200

    Correção do Teste

--- src/main/java/hello/Teste.java
+++ src/main/java/hello/Teste.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 
 public class Teste {
 	
-	final static void main(String args[]) { 
+	public static void main(String args[]) { 
 		
 		Sistema sistema = new Sistema();
 		
