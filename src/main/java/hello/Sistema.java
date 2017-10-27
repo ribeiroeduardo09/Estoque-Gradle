aae58fae75a42b55d115a0cc3d88f4d8750cd793
Eduardo Pereira Ribeiro <ribeiroeduardo09@gmail.com>
2017-10-27 21:14:29 -0200

    Inserção dos métodos "Produto" e "Fornecedor" (na classe "Sistema") no banco de dados.

--- src/main/java/hello/Sistema.java
+++ src/main/java/hello/Sistema.java
@@ -5,147 +5,242 @@ import java.util.List;
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Query;
 
 public class Sistema {
-	
+
 	/*---------Import do Banco de Dados----------*/
-	ObjectContainer students = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/students.db4o");
-	ObjectContainer questions = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/questions.db4o");
-	ObjectContainer competencies = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/competencies.db4o");
-	ObjectContainer institutions = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/institutions.db4o");
-	ObjectContainer psychologists = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/psychologists.db4o");
+	ObjectContainer products = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/products.db4o");
+	ObjectContainer providers = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/providers.db4o");
+	ObjectContainer users = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/users.db4o");
+	ObjectContainer purchases = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/purchases.db4o");
 	ObjectContainer adms = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "bd/adms.db4o");
-	
+
 	/*---------Declaração----------*/
 	private List<Produto> produtos = new LinkedList<Produto>();
 	private List<Fornecedor> fornecedores = new LinkedList<Fornecedor>();
 	private List<Compra> compras = new LinkedList<Compra>();
 	private List<Usuario> usuarios = new LinkedList<Usuario>();
-	
+
 	/*---------Produtos----------*/
-	public void addProduto(Produto prod){
-		produtos.add(prod);
+	public boolean addProduto(Produto prod) {
+
+		if (isProuctAvailable(prod.getCodigoproduto())) {
+
+			products.store(prod);
+			products.commit();
+
+			return true;
+		}
+		return false;
 	}
-	public void delProduto(int codigoproduto){
-		for(Produto prod:produtos){
-			if(prod.getCodigoproduto()==codigoproduto)
-				produtos.remove(prod);
+
+	public void delProduto(int codigoproduto) {
+		
+		Query query = products.query();
+		query.constrain(Produto.class);
+		List<Produto> allProducts = query.execute();
+
+		for (Produto prod : allProducts) {
+			if (prod.getCodigoproduto() == codigoproduto)
+				products.delete(codigoproduto);
+				products.commit();
 		}
 	}
-	public Produto searchProduto(int codigoproduto){
-		for(Produto prod:produtos){
-			if(prod.getCodigoproduto() == codigoproduto)
-				return prod;
-		} 
+
+	public Produto searchProduto(int codigoproduto) {
+
+		Query query = products.query();
+		query.constrain(Produto.class);
+		ObjectSet<Produto> allProducts = query.execute();
+
+		for (Produto produto : allProducts) {
+			if (produto.getCodigoproduto() == codigoproduto) {
+				return produto;
+			}
+		}
 		return null;
 	}
+
+	public boolean isProuctAvailable(int codigoproduto) {
+		
+		Query query = products.query();
+		query.constrain(Produto.class);
+		ObjectSet<Produto> allProducts = query.execute();
+
+		for (Produto produto : allProducts) {
+			if (produto.getCodigoproduto() == (codigoproduto))
+				return false;
+		}
+		return true;
+	}
+
 	/*---------Fornecedores----------*/
-	public void addFornecedor(Fornecedor fornecedor){
-		fornecedores.add(fornecedor);
+	public boolean addFornecedor(Fornecedor fornecedor) {
+		
+		if (isProviderAvailable(fornecedor.getCnpj())) {
+
+			providers.store(fornecedor);
+			providers.commit();
+
+			return true;
+		}
+		return false;
 	}
-	public void delFornecedor(int cnpj){
-		for(Fornecedor fncdr:fornecedores){
-			if(fncdr.getCnpj()==cnpj)
-				fornecedores.remove(fncdr);
+
+	public void delFornecedor(int cnpj) {
+
+		Query query = providers.query();
+		query.constrain(Fornecedor.class);
+		List<Fornecedor> allProviders = query.execute();
+
+		for (Fornecedor fornecedor : allProviders) {
+			if (fornecedor.getCnpj() == cnpj)
+				providers.delete(cnpj);
+				providers.commit();
 		}
 	}
-	public Fornecedor searchFornecedor(int cnpj){
-		for(Fornecedor fncdr:fornecedores){
-			if(fncdr.getCnpj()==cnpj)
-				return fncdr;
+
+	public Fornecedor searchFornecedor(int cnpj) {
+		
+		Query query = providers.query();
+		query.constrain(Fornecedor.class);
+		ObjectSet<Fornecedor> allProviders = query.execute();
+
+		for (Fornecedor fornecedor : allProviders) {
+			if (fornecedor.getCnpj() == cnpj) {
+				return fornecedor;
+			}
 		}
 		return null;
 	}
-	public LinkedList<Produto> searchFornecedorListP(int cnpj){
+
+	public LinkedList<Produto> searchFornecedorListP(int cnpj) { //TODO Passar este método para o Banco de Dados.
 		LinkedList<Produto> lTemp = new LinkedList<Produto>();
 		Fornecedor forn = null;
-		for(Fornecedor tempForn:fornecedores){
-				if(tempForn.getCnpj() == cnpj)
-					forn = tempForn;
+		for (Fornecedor tempForn : fornecedores) {
+			if (tempForn.getCnpj() == cnpj)
+				forn = tempForn;
 		}
-		for(Produto tempProd:produtos){
-			for(Produto tempProd2:forn.getProdutos()){
-				if(tempProd.getCodigoproduto() == tempProd2.getCodigoproduto())
+		for (Produto tempProd : produtos) {
+			for (Produto tempProd2 : forn.getProdutos()) {
+				if (tempProd.getCodigoproduto() == tempProd2.getCodigoproduto())
 					lTemp.add(tempProd);
 			}
 		}
 		return lTemp;
 	}
 	
-	public LinkedList<Fornecedor> searchFornecedorNome(String nomeFornecedor){
-		LinkedList<Fornecedor> fornList = new LinkedList<Fornecedor>();
-		for(Fornecedor fornTemp:fornecedores) {
-			if(fornTemp.getNome().toLowerCase().contains(nomeFornecedor.toLowerCase())) {
-				fornList.add(fornTemp);
+	
+
+	public List<Fornecedor> searchFornecedorNome(String nomeFornecedor) {
+		
+		List<Fornecedor> fornList = new LinkedList<Fornecedor>();
+		
+		Query query = providers.query();
+		query.constrain(Fornecedor.class);
+		ObjectSet<Fornecedor> allProviders = query.execute();
+		
+		for (Fornecedor fornecedor : allProviders) {
+			if (fornecedor.getNome().toLowerCase().contains(nomeFornecedor.toLowerCase())) {
+				fornList.add(fornecedor);
 			}
 		}
+		
 		return fornList;
 	}
-	
+
+	public boolean isProviderAvailable(int cnpj) {
+		
+		Query query = providers.query();
+		query.constrain(Fornecedor.class);
+		ObjectSet<Fornecedor> allProviders = query.execute();
+
+		for (Fornecedor fornecedor : allProviders) {
+			if (fornecedor.getCnpj() == (cnpj))
+				return false;
+		}
+		return true;
+	}
+
 	/*---------Compras----------*/
-	public void addCompra(Compra compra){
+	public void addCompra(Compra compra) {
 		compras.add(compra);
 	}
-	public void delCompra(int codigocompra){
-		for(Compra comp:compras){
-			if(comp.getCodigocompra()==codigocompra)
+
+	public void delCompra(int codigocompra) {
+		for (Compra comp : compras) {
+			if (comp.getCodigocompra() == codigocompra)
 				compras.remove(comp);
 		}
 	}
-	public Compra searchCompra(int codigocompra){
-		for(Compra comp:compras){
-			if(comp.getCodigocompra()==codigocompra)
+
+	public Compra searchCompra(int codigocompra) {
+		for (Compra comp : compras) {
+			if (comp.getCodigocompra() == codigocompra)
 				return comp;
 		}
 		return null;
 	}
+
 	/*---------Usuários----------*/
-	public void addUsuario(Usuario usuario){
+	public void addUsuario(Usuario usuario) {
 		usuarios.add(usuario);
 	}
-	public void delUsuario(String login){
-		for(Usuario usr:usuarios){
-			if(usr.getLogin().equals(login))
+
+	public void delUsuario(String login) {
+		for (Usuario usr : usuarios) {
+			if (usr.getLogin().equals(login))
 				usuarios.remove(usr);
 		}
 	}
-	public Usuario searchUsuario(String login){
-		for(Usuario usr:usuarios){
-			if(usr.getLogin().equals(login))
+
+	public Usuario searchUsuario(String login) {
+		for (Usuario usr : usuarios) {
+			if (usr.getLogin().equals(login))
 				return usr;
 		}
 		return null;
 	}
-	public boolean logarUsuario(String login, String senha){
-		for(Usuario usr:usuarios){
-			if(usr.getLogin().equals(login) && usr.getSenha().equals(senha))
+
+	public boolean logarUsuario(String login, String senha) {
+		for (Usuario usr : usuarios) {
+			if (usr.getLogin().equals(login) && usr.getSenha().equals(senha))
 				return true;
 		}
 		return false;
 	}
-	
+
 	/*---------Getters e setters----------*/
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
+
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
 	}
+
 	public List<Fornecedor> getFornecedores() {
 		return fornecedores;
 	}
+
 	public void setFornecedores(List<Fornecedor> fornecedores) {
 		this.fornecedores = fornecedores;
 	}
+
 	public List<Compra> getCompras() {
 		return compras;
 	}
+
 	public void setCompras(List<Compra> compras) {
 		this.compras = compras;
 	}
+
 	public List<Usuario> getUsuarios() {
 		return usuarios;
 	}
+
 	public void setUsuarios(List<Usuario> usuarios) {
 		this.usuarios = usuarios;
 	}
