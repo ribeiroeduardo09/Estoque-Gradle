17a16df49185518d0e5ff0cd9c99d6f401c9a0d3
filiperdt <rubenfilipeao@gmail.com>
2017-11-08 17:18:43 -0200

    Teste retorno de objetos

--- src/main/java/hello/MainServer.java
+++ src/main/java/hello/MainServer.java
@@ -2,6 +2,10 @@ package hello;
 
 import static spark.Spark.*;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.LinkedList;
+
 
 public class MainServer {
 	
@@ -48,7 +52,14 @@ public class MainServer {
     }
     
     public static void inicializarProdutos() {};
-    public static void inicializarFornecedores() {};
+    public static void inicializarFornecedores(){
+    	LinkedList<Produto> lista = new LinkedList<Produto>();
+    	lista.add(sistema.searchProduto(01));
+    	lista.add(sistema.searchProduto(02));
+    	System.out.println(lista.get(0).getCodigoproduto());
+    	System.out.println(lista.get(1).getCodigoproduto());
+    	sistema.addFornecedor(new Fornecedor("Apple", 4444, "a@gmail.com", "1111-1111", "Rua Alta", LocalDate.of(2017, Month.AUGUST, 18), lista));
+    }
     public static void inicializarCompras() {};
     public static void inicializarUsuarios() {};
     
